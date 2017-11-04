48ca2c93567fb5a76f1bf07989671a63936c4bb8
Pete Jemian <prjemian@gmail.com>
2017-11-03 19:50:47 -0500

    reconfigure the area detector setup for AD2.5 sim detector
    STILL, cannot find the image.  Hrmph.  databroker setup still broken?

--- startup/70-areadetector.py
+++ startup/70-areadetector.py
@@ -1,62 +1,66 @@
 print(__file__)
 
 from ophyd import SingleTrigger, AreaDetector, SimDetector
-from ophyd import HDF5Plugin
+from ophyd.areadetector.plugins import HDF5Plugin
+from ophyd.areadetector.trigger_mixins import SingleTrigger
 from ophyd.areadetector.filestore_mixins import FileStoreHDF5IterativeWrite
 from ophyd import Component, Device, EpicsSignalWithRBV
 from ophyd.areadetector import ADComponent
 
 
-class MyHDF5Plugin(HDF5Plugin, FileStoreHDF5IterativeWrite):
-	
-	file_number_sync = None
-	
-	def get_frames_per_point(self):
-		return self.parent.cam.num_images.get()
-	
+image_file_path = "/tmp"
 
-class MyDetector(SingleTrigger, SimDetector):
-	
-	hdf1 = Component(
-		MyHDF5Plugin, 
-		"HDF1:", 
-		root="/", 					# for databroker filestore
-		write_path_template="/tmp",	# for EPICS area detector
-		)
+
+class MyHDF5Plugin(HDF5Plugin, FileStoreHDF5IterativeWrite):
+    
+    file_number_sync = None     # AD 2.5
+    
+    def get_frames_per_point(self):     # AD 2.5
+        return self.parent.cam.num_images.get()
+
+
+class MyHdf5Detector(SimDetector, SingleTrigger):
+    
+    hdf1 = Component(
+        MyHDF5Plugin,
+        'HDF1:', 
+        root='/',                               # for databroker
+        write_path_template=image_file_path,    # for EPICS AD,
+        reg=db.reg
+    )
+
 
 try:
-	simdet = MyDetector('13SIM1:', name='simdet')
-	simdet.read_attrs = ['hdf1', 'cam']
-	simdet.hdf1.read_attrs = []  # 'image' gets added dynamically
-	#simdet = SimDetector('13SIM1:')
+
+    simdet = MyHdf5Detector('13SIM1:', name='simdet')
+    simdet.read_attrs = ['hdf1', 'cam']
+    simdet.hdf1.read_attrs = []  # 'image' gets added dynamically
+
 except TimeoutError:
-	print("Could not connect 13SIM1: sim detector")
+    print("Could not connect 13SIM1: sim detector")
 
 """
 example::
 
-	RE(count([simdet], num=1))
-	imgs = db.get_images(db[-1], 'simdet_image')
-	imgs[0][0]
-	data_uid = simdet.read()['simdet_image']['value']
-	fs.retrieve(data_uid)                                                    
+    simdet.describe_configuration()
+    RE(bp.count([simdet]))
+    cfg = simdet.hdf1.read_configuration()
+    file_name = cfg["simdet_hdf1_full_file_name"]['value']
+    cfg.get('simdet_image', 'not found')
+    data_uid = cfg['simdet_image']['value']
+    db.reg.retrieve(data_uid)                                                    
 
-
-	In [15]: %history
-	simdet.describe()
-	RE(count([simdet]))
-	db.get_images(db[-1], 'simdet_image')
-	images = _
-	images[0]
-	images[0][0]
-	simdet.cam.num_images.set(2)
-	RE(count([simdet]))
-	db.get_images(db[-1], 'simdet_image')
-	images = _
-	images[0][0]
-	images[0]
-	images[0][1]
-	%history
+    db[-1].data("simdet_image")
+    images = list(_)
+    images[0]
+    images[0][0]
+    simdet.cam.num_images.set(2)
+    RE(bp.count([simdet]))
+    db[-1].data("simdet_image")
+    images = list(_)
+    images[0][0]
+    images[0]
+    images[0][1]
 
 
 """
