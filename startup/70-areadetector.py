0562478b69c11154ba314e9985fb468b6757b477
Pete Jemian <prjemian@gmail.com>
2017-11-06 12:57:59 -0600

    resolve setup questions

--- startup/70-areadetector.py
+++ startup/70-areadetector.py
@@ -23,7 +23,7 @@ class MyHdf5Detector(SimDetector, SingleTrigger):
     
     hdf1 = Component(
         MyHDF5Plugin,
-        'HDF1:', 
+        suffix='HDF1:', 
         root='/',                               # for databroker
         write_path_template=image_file_path,    # for EPICS AD,
         reg=db.reg
@@ -35,9 +35,15 @@ try:
     simdet = MyHdf5Detector('13SIM1:', name='simdet')
     simdet.read_attrs = ['hdf1', 'cam']
     simdet.hdf1.read_attrs = []  # 'image' gets added dynamically
+    # put these things in each event document
+    # only first 3 characters show in the LiveTable callback.  So what?
+    simdet.hdf1.read_attrs.append("file_name")
+    simdet.hdf1.read_attrs.append("file_path")
+    simdet.hdf1.read_attrs.append("full_file_name")
 
 except TimeoutError:
     print("Could not connect 13SIM1: sim detector")
+
 
 """
 example::
@@ -46,21 +52,7 @@ example::
     RE(bp.count([simdet]))
     cfg = simdet.hdf1.read_configuration()
     file_name = cfg["simdet_hdf1_full_file_name"]['value']
-    cfg.get('simdet_image', 'not found')
-    data_uid = cfg['simdet_image']['value']
-    db.reg.retrieve(data_uid)                                                    
-
-    db[-1].data("simdet_image")
-    images = list(_)
-    images[0]
-    images[0][0]
-    simdet.cam.num_images.set(2)
-    RE(bp.count([simdet]))
-    db[-1].data("simdet_image")
-    images = list(_)
-    images[0][0]
-    images[0]
-    images[0][1]
-
+    for ev in db[-1].events():
+        print(ev["data"][simdet.hdf1.name+"_full_file_name"])
 
 """
