9dace085a11cd5f45c0a4538680ff7d1a12918ef
Pete Jemian <prjemian@gmail.com>
2017-11-07 17:10:39 -0600

    demonstrate 0MQ with BlueSky

--- startup/80-callbacks.py
+++ startup/80-callbacks.py
@@ -33,3 +33,40 @@ class CustomCallbackCollector(object):
 
 cb_collector = CustomCallbackCollector()
 callback_db['cb_collector'] = RE.subscribe(cb_collector.receiver)
+
+
+from prj_support.zmq_pair import ZMQ_Pair
+
+class MyCallback0MQ(object):
+    """
+    My BlueSky 0MQ talker to send *all* documents emitted
+    """
+    
+    eot_signal_text = b"END OF TRANSMISSION"
+    
+    def __init__(self, host=None, port=None, detector=None):
+        self.talker = ZMQ_Pair(host or "localhost", port or "5556")
+        self.detector = detector
+    
+    def end(self):
+        self.talker.send_string(self.talker.eot_signal_text.decode)
+
+    def receiver(self, key, document):
+        """receive from RunEngine, send from 0MQ talker"""
+        self.talker.send_string(key)
+        self.talker.send_string(document)
+        if key == "event" and self.detector is not None:
+            # Is it faster to pick this up by EPICS CA?
+            # Using 0MQ, no additional library is needed
+            self.talker.send_string("rank")
+            self.talker.send_string(str(len(self.detector.image.shape)))
+            self.talker.send_string("shape")
+            self.talker.send_string(str(self.detector.image.shape))
+            self.talker.send_string("image")
+            self.talker.send(self.detector.image)
+
+try:
+    zmq_talker = MyCallback0MQ(detector=plainsimdet.image)
+    callback_db['zmq_talker'] = RE.subscribe(zmq_talker.receiver)
+except Exception:
+    pass
