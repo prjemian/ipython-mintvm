fc475ab391b439c1f877ebe5c08438a0a6bed7d6
Pete Jemian <prjemian@gmail.com>
2017-11-21 14:36:43 -0600

    simulated detector

--- startup/20-detectors.py
+++ startup/20-detectors.py
@@ -3,8 +3,9 @@ print(__file__)
 from ophyd import (EpicsScaler, EpicsSignal, EpicsSignalRO,
                    Device, DeviceStatus)
 from ophyd import Component as Cpt
-
 import time
+from APS_BlueSky_tools.examples import SynPseudoVoigt
+
 
 #aps_current = EpicsSignalRO("S:SRcurrentAI", name="aps_current")
 
@@ -12,3 +13,14 @@ import time
 #bs_bm2 = EpicsSignalRO('BL14B:Det:BM2', name='bs_bm2')
 noisy = EpicsSignalRO('prj:userCalc1', name='noisy')
 scaler = EpicsScaler('prj:scaler1', name='scaler')
+
+
+synthetic_pseudovoigt = SynPseudoVoigt(
+    'synthetic_pseudovoigt', m1, 'm1', 
+    center=-1.5 + 0.5*np.random.uniform(), 
+    eta=0.2 + 0.5*np.random.uniform(), 
+    sigma=0.001 + 0.05*np.random.uniform(), 
+    scale=1e5,
+    bkg=0.01*np.random.uniform())
+
+#  RE(bp.scan([synthetic_pseudovoigt], m1, -2, 0, 219))
