93deba0fa25f6649453291f8ca73633cdbe5de5f
Pete Jemian <prjemian@gmail.com>
2017-11-21 14:28:00 -0600

    revised metadata

--- startup/60-metadata.py
+++ startup/60-metadata.py
@@ -18,7 +18,7 @@ def print_scan_ids(name, start_doc):
 
 # Set up default metadata
 
-RE.md['beamline_id'] = 'developer__YOUR_BEAMLINE_HERE'
+RE.md['beamline_id'] = 'developer'  # TODO: !!!YOUR BEAMLINE HERE!!!
 RE.md['proposal_id'] = None
 RE.md['pid'] = os.getpid()
 
@@ -29,7 +29,7 @@ HOSTNAME = socket.gethostname() or 'localhost'
 USERNAME = getpass.getuser() or 'synApps_xxx_user' 
 RE.md['login_id'] = USERNAME + '@' + HOSTNAME
 
-import os
-for key, value in os.environ.items():
-    if key.startswith("EPICS"):
-        RE.md[key] = value
+#import os
+#for key, value in os.environ.items():
+#    if key.startswith("EPICS"):
+#        RE.md[key] = value
