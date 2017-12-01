6601e1cc0861e72fd0640fb8f53e551ae09761eb
Pete Jemian <prjemian@gmail.com>
2017-12-01 16:13:03 -0600

    make into a demo

--- startup/50-plans.py
+++ startup/50-plans.py
@@ -2,13 +2,13 @@ print(__file__)
 
 # Bluesky plans (scans)
 
-if False:       # demo & testing code
+def demo_tuning():       # demo & testing code
     simulate_peak(calc1, m1, profile="lorentzian")
     RE(bp.tune_centroid([noisy], "noisy", m1, -2, 0, 0.00001, 10, snake=True))
     
     RE(
         bp.tune_centroid(
-            [synthetic_pseudovoigt], "synthetic_pseudovoigt", m1, 
+            [spvoigt], "spvoigt", m1, 
             -2, 0, 0.00001, 10, snake=True
         ),
         # these two styles go to the LivePlot()
@@ -17,7 +17,7 @@ if False:       # demo & testing code
     )
     RE(
         bp.adaptive_scan(
-            [synthetic_pseudovoigt], 'synthetic_pseudovoigt', m1, 
+            [spvoigt], 'spvoigt', m1, 
             start=-2, stop=0, min_step=0.01, max_step=1, 
             target_delta=500, backstep=True
         )
