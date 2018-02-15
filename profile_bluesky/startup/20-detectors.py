9e3583846a5547504d486185aabf32747da95411
Pete Jemian <prjemian@gmail.com>
2018-02-15 15:22:22 -0600

    demo how to use ScalerCH

--- profile_bluesky/startup/20-detectors.py
+++ profile_bluesky/startup/20-detectors.py
@@ -4,10 +4,28 @@ print(__file__)
 
 from APS_BlueSky_tools.examples import SynPseudoVoigt
 
+# this is DEMO work only
+# do these before connecting (autosave not configured yet)
+_xref = dict(
+    NM1 = 'clock',
+    NM2 = 'I0',
+    NM3 = 'I',
+    NM4 = 'other',
+    NM5 = 'other',
+    NM6 = 'one space',
+    NM8 = 'many    spaces',
+)
+_channel_names = []
+#for k, v in _xref.items():
+#    epics.caput('prj:scaler1.'+k, v)
+#    k = 'chan%02d' % int(k.lstrip('NM'))
+#    _channel_names.append(k)
+
 noisy = EpicsSignalRO('prj:userCalc1', name='noisy')
 scaler = EpicsScaler('prj:scaler1', name='scaler')
-#sc2 = ScalerCH('prj:scaler1', name='sc2')
-
+sc2 = ScalerCH('prj:scaler1', name='sc2')
+#sc2.channels.read_attrs = _channel_names
+#sc2.channels.configuration_attrs = _channel_names
 
 spvoigt = SynPseudoVoigt(
     'spvoigt', m1, 'm1', 
